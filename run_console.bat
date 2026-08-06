89a02ff4d2c4581a387d34fad5ee45d2d543e611
chaehoon092900-stack <chaehoon092900@gmail.com>
2026-08-06 10:33:22 +0900

    Delete run_console.bat

--- run_console.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-title Broadcast Console Server
-echo Starting Broadcast Console Server...
-python -m pip install flask requests --quiet
-start http://localhost:5000
-python server.py
-pause
